761b2e55ceca54533186936e2e96b6b9f9c02954
thp-mmo <tranhuuphuc1242006@gmail.com>
2026-05-19 23:51:09 +0700

    Rename SodokuBatteOnline.sln file

--- Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+758f5d5d4db1af7ac41d005ae87fd78d6b87e91d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34e90196e97f121d03bc4e3a079cca276d0b15e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
