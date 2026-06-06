01861d468386ac04e0104bacb8dd54a753ec5fa2
GiangTy <giangtyit@gmail.com>
2026-06-07 01:36:46 +0700

    "feat(server/network): thiet lap cau truc thu muc mang va hoan thanh ket noi TCP bat dong bo co ban"

--- src/SudokuBattleOnline/Server/Program.cs
+++ src/SudokuBattleOnline/Server/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using SudokuBattle.Server.Network;
+
+namespace SudokuBattle.Server
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Đồng bộ định dạng hiển thị ký tự UTF-8 trên Console
+            Console.OutputEncoding = Encoding.UTF8;
+
+            TcpServer server = new TcpServer();
+            // Kích hoạt Server chạy bất đồng bộ
+            await server.StartAsync();
+        }
+    }
+}
